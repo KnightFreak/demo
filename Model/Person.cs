8b7ce56f28d675f96e0fb47ef5a63000eae54e19
KnightFreak <30434525+KnightFreak@users.noreply.github.com>
2017-08-14 17:07:32 +0530

    Update Person.cs

--- Model/Person.cs
+++ Model/Person.cs
@@ -1,15 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
 
+using System.Data.SQLite;
+
 namespace first_mvvm.Model
 {
     public class Person : INotifyPropertyChanged  //notify to UI
     {
-        private string fname, lname, fullname, email, dob, userid, number;
+        private string fname, lname, fullname, email, dob, userid, number, psw1;
 
         public String Fname
         {
@@ -88,6 +90,32 @@ namespace first_mvvm.Model
                 //OnPropertyChanged(Number);
             }
         }
+
+        public String Password
+        {
+            get
+            { return psw1; }
+            set
+            {
+                psw1 = value;
+                //OnPropertyChanged(Number);
+            }
+        }
+        
+        //public void create_table()
+        //{
+        //    if (!System.IO.File.Exists("Registration.db"))
+        //    {
+        //        Console.WriteLine("Just entered to create database");
+        //        SQLiteConnection.CreateFile("Registration.db");
+        //        SQLiteConnection db_connection = new SQLiteConnection("Data Source=Registration.db;Version=3;");
+        //        db_connection.Open();
+        //        string sql = "create table PatientInfo(FirstName varchar(20), LastName varchar(20), FullName varchar(20), EmailId varchar(30), Dob varchar(20), ContactNumber varchar(10), UserId varchar(20), Password varchar(20))";
+        //        SQLiteCommand command = new SQLiteCommand(sql, db_connection);
+        //        command.ExecuteNonQuery();
+        //        db_connection.Close();
+        //    }
+        //}       
 
         public event PropertyChangedEventHandler PropertyChanged;
         private void OnPropertyChanged(string p)
