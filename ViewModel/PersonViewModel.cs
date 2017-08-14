2db1863b9185bf43d625cdef7061c61609329fd5
KnightFreak <30434525+KnightFreak@users.noreply.github.com>
2017-08-14 17:03:45 +0530

    Update PersonViewModel.cs

--- ViewModel/PersonViewModel.cs
+++ ViewModel/PersonViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -15,21 +15,59 @@ using System.IO;
 using System.Data.SQLite;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Security.Cryptography;
 
 namespace first_mvvm.ViewModel
 {
     class PersonViewModel : INotifyPropertyChanged
     {
-        bool add_update_flg = false;
+        private string _LoginEmail;
+        public String LoginEmail
+        {
+            get
+            { return _LoginEmail; }
+            set
+            {
+                _LoginEmail = value;
+                OnPropertyChanged("LoginEmail");
+            }
+        }
 
+        /*************************************************************************************************/   
+        bool add_update_flg = false;
+        /*************************************************************************************************/
         public PersonViewModel()
-       {
+        {
            Person = new Person();
-           Persons = Load_Info.load_complete_info();
+           if (!System.IO.File.Exists("Registration.db"))
+           {
+               Console.WriteLine("Just entered to create database");
+               SQLiteConnection.CreateFile("Registration.db");
+               SQLiteConnection db_connection = new SQLiteConnection("Data Source=Registration.db;Version=3;");
+               db_connection.Open();
+               string sql = "create table PatientInfo(FirstName varchar(20), LastName varchar(20), FullName varchar(20), EmailId varchar(30), Dob varchar(20), ContactNumber varchar(10), UserId varchar(20), Password varchar(20))";
+               SQLiteCommand command = new SQLiteCommand(sql, db_connection);
+               command.ExecuteNonQuery();
+               db_connection.Close();
+           }
+           SQLiteConnection connection = new SQLiteConnection("Data Source=Registration.db;Version=3;");
+           string query = "select * from PatientInfo";
+           connection.Open();
+           SQLiteCommand commandtxt = new SQLiteCommand(query, connection);
+           SQLiteDataReader reader = commandtxt.ExecuteReader();
+
+           Persons = new ObservableCollection<Person>();
+           while(reader.Read())
+           {
+               Person p = new Person { Fname = (string)reader["FirstName"], Lname = (string)reader["LastName"], Email_id = (string)reader["EmailId"], Dob = (string)reader["Dob"], UserID = (string)reader["UserId"], Number = (string)reader["ContactNumber"], Password = (string)reader["Password"]};// 
+               Persons.Add(p);
+           }
+           connection.Close();
        }
-       
-       private Person _person;
-       public Person Person
+         
+        /************************************************************************************************/       
+        private Person _person;
+        public Person Person
        {
            get { return _person; }
            set
@@ -40,17 +78,16 @@ namespace first_mvvm.ViewModel
        }
 
         /************************************************************************************************/
-
         private ObservableCollection<Person> _persons;
         public ObservableCollection<Person> Persons
-        {
-            get { return _persons; }
-            set
-            {
-                _persons = value;
-                OnPropertyChanged("Persons");
-            }
-        }
+       {
+           get { return _persons; }
+           set
+           {
+               _persons = value;
+               OnPropertyChanged("Persons");
+           }
+       }
 
         /*************************************************************************************************/
         public Person _Selectedperson;
@@ -64,6 +101,7 @@ namespace first_mvvm.ViewModel
                 if (SelectedPerson != null)
                 {
                     ButtonText = "Update";
+
                     for (int i = 0; i < Persons.Count; i++)
                     {
                         if (Persons[i].UserID == SelectedPerson.UserID)
@@ -75,6 +113,7 @@ namespace first_mvvm.ViewModel
                     }
                     //Change the flag value for ADD_UPDATE function. Update => true. Add => false.
                     add_update_flg = true;
+                    
                 }
             }
         }
@@ -91,6 +130,8 @@ namespace first_mvvm.ViewModel
                 OnPropertyChanged("ButtonText");
             }
         }
+
+        /**************************************************************************************************/
         public ICommand AddCommand
         {
             get
@@ -103,16 +144,9 @@ namespace first_mvvm.ViewModel
             }
         }
 
-
         private void AddExecute(object parameter)
         {
-            var values = (object[])parameter;
-            var passwordBox1 = values[0] as PasswordBox;
-            var passworda = passwordBox1.Password;
-            var passwordBox2 = values[1] as PasswordBox;
-            var passwordb = passwordBox2.Password;
-            Console.WriteLine(passworda);
-            Console.WriteLine(passwordb);
+            
             //START VALIDATION
             if (String.IsNullOrEmpty(Person.Fname) || !Regex.IsMatch(Person.Fname, @"^[a-zA-Z]+$"))
             {
@@ -140,14 +174,25 @@ namespace first_mvvm.ViewModel
                 return;
             }
 
-            /******/
-            //var passwordBox = parameter as PasswordBox;
-            //var password = passwordBox.Password;
-            //Console.WriteLine(password);
-            /******/
-             
+            var pswBoxes = parameter as List<object>;
+            PasswordBox pb1, pb2;
+            pb1 = pswBoxes[0] as PasswordBox;
+            string psw0 = pb1.Password;
+            pb2 = pswBoxes[1] as PasswordBox;
+            string psw1 = pb2.Password;
+
+            if (psw0 != psw1)
+            {
+                MessageBox.Show("Passwords didn't match. Enter again!");
+                return;
+            }
+            else
+            {
+                Person.Password = SHA.GenerateSHA256String(psw0);
+            }     
+
             /********************************************************************************************************/
-            //LOAD DATA FROM XML DOCUMENT TO CHECK WHETHER SAME INFO EXISTS
+            //CHECK WHETHER SAME INFO EXISTS
             for (int i = 0; i < Persons.Count; i++)
             {
                 if (Persons[i].Fname == Person.Fname && Persons[i].Lname == Person.Lname
@@ -158,14 +203,25 @@ namespace first_mvvm.ViewModel
                 }
             }
 
-            if (!add_update_flg)
+            if (!add_update_flg)//perform add function
             {
                 Person.UserID = DateTime.Now.ToString("yyyyMMddHHmmss");
                 Persons.Add(Person);
+                SQLiteConnection db_connection = new SQLiteConnection("Data Source=Registration.db;Version=3;");
+                db_connection.Open();
+                string Query = "insert into PatientInfo(FirstName, LastName, FullName, EmailId, Dob, ContactNumber, UserId, Password) values('" + Person.Fname + "','" + Person.Lname + "','" + Person.FullName + "','" + Person.Email_id + "', '" + Person.Dob + "', '" + Person.Number + "','" + Person.UserID + "', '" + Person.Password + "')";
+                SQLiteCommand command = new SQLiteCommand(Query, db_connection);
+                command.ExecuteNonQuery();
+                db_connection.Close();
+                pb1.Password = "";
+                pb2.Password = "";
             }
 
             if(add_update_flg) // perform update function
             {
+                var temp = Person.UserID;
+
+                //Update Collection
                 for (int i = 0; i < Persons.Count; i++)
                 {
                     if (Persons[i].UserID == Person.UserID)
@@ -176,19 +232,16 @@ namespace first_mvvm.ViewModel
                         break;
                     }
                 }
+                SQLiteConnection db_connection = new SQLiteConnection("Data Source=Registration.db;Version=3;");
+                db_connection.Open();
+                string Query = "update PatientInfo set FirstName = '" + Person.Fname + "',LastName = '" + Person.Lname + "',FullName = '" + Person.FullName + "',EmailId = '" + Person.Email_id + "', Dob = '" + Person.Dob + "', ContactNumber = '" + Person.Number + "',UserId = '" + Person.UserID + "' where UserId = '" + temp + "' ";
+                SQLiteCommand command = new SQLiteCommand(Query, db_connection);
+                command.ExecuteNonQuery();
+                db_connection.Close();
             }
 
-            
-            
-            /*********************************************************************************************************/
-            //SAVE DATA TO XML DOCUMENT
-            XmlSerializer xmls = new XmlSerializer(typeof(ObservableCollection<Person>));
-            //File.Delete("data1.xml");
-            using (Stream s = File.OpenWrite("data1.xml"))
-            {
-                xmls.Serialize(s, Persons);
-            }
             /**********************************************************************************************************/
+
             if(add_update_flg)
             {
                 MessageBox.Show("Data Updated Successfully");
@@ -205,7 +258,6 @@ namespace first_mvvm.ViewModel
         }
 
         /***************************************************************************************************/
-
         private ICommand _DelCommand;
         public ICommand DelCommand
         {
@@ -228,25 +280,28 @@ namespace first_mvvm.ViewModel
         }
 
         private void DelExecute(object parameter)
-        {
+        {          
+            SQLiteConnection db_connection = new SQLiteConnection("Data Source=Registration.db;Version=3;");
+            db_connection.Open();
+            string Query = "delete from PatientInfo where UserId = '" + SelectedPerson.UserID + "'";
+            SQLiteCommand command = new SQLiteCommand(Query, db_connection);
+            command.ExecuteNonQuery();
+            MessageBox.Show("Deleted Successfully");
+            db_connection.Close();
+
             for (int i = 0; i < Persons.Count; i++)
             {
                 if (Persons[i].UserID == SelectedPerson.UserID)
                 {
                     Persons.RemoveAt(i);
-                    File.Delete("data1.xml");
-                    XmlSerializer xmlser = new XmlSerializer(typeof(ObservableCollection<Person>));
-                    using (Stream s = File.OpenWrite("data1.xml"))
-                    {
-                        xmlser.Serialize(s, Persons);
-                    }
                     ButtonText = "Add";
                     Person = new Person();
+                    add_update_flg = false;
                     break;
                 }
-
             }
         }
+
         /******************************************************************************************************/
         private ICommand _RegNewCommand;
         public ICommand RegNewCommand
@@ -268,6 +323,13 @@ namespace first_mvvm.ViewModel
             //update the flg for Add function
             ButtonText = "Add";
             add_update_flg = false;
+            //Reset Password boxes
+            var pswBoxes = parameter as List<object>;
+            PasswordBox pb1, pb2;
+            pb1 = pswBoxes[0] as PasswordBox;
+            pb2 = pswBoxes[1] as PasswordBox;
+            pb1.Password = "";
+            pb2.Password = "";
         }
 
         private bool CanRegNewExecute(object parameter)
@@ -291,9 +353,25 @@ namespace first_mvvm.ViewModel
 
         private void LoginExecute(object parameter)
         {
-            //var passwordBox = parameter as PasswordBox;
-            //var password = passwordBox.Password;
 
+            var passwordBox = parameter as PasswordBox;
+            var password = passwordBox.Password;
+            var hashed_password = SHA.GenerateSHA256String(password);
+
+
+            for (int i = 0; i < Persons.Count; i++)
+            {
+                if (Persons[i].Email_id == LoginEmail && Persons[i].Password == hashed_password)
+                {
+                    MessageBox.Show("Successfully logged in!");
+                    LoginEmail = "";
+                    passwordBox.Password = "";
+                    return;
+                }  
+            }
+            MessageBox.Show("Incorrect Username or Password");
+            passwordBox.Password = "";
+            return;   
         }
 
         private bool CanLoginExecute(object parameter)
